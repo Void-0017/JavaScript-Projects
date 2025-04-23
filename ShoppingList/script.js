00b4d029c218981adbbe8c809c6d428127d90519
Void-0017 <shivamgaonkar17@gmail.com>
2025-04-23 14:49:58 +0530

    Shopping List #2 Changes

--- ShoppingList/script.js
+++ ShoppingList/script.js
@@ -1,4 +1,25 @@
 const itemInput = document.querySelector(".btn");
+const closeIcons = document.querySelectorAll("i");
+const clearItems = document.querySelector("#clear");
+
+clearItems.addEventListener("click", () => {
+  const itemList = document.querySelector("ul");
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+});
+
+closeIcons.forEach((icon) => {
+  icon.addEventListener("click", removeItem);
+});
+
+function removeItem(e) {
+  // console.log(e.currentTarget.parentNode.parentNode);
+  document
+    .querySelector("ul")
+    .removeChild(e.currentTarget.parentNode.parentNode);
+}
+
 itemInput.addEventListener("click", (e) => {
   e.preventDefault();
   const val = document.querySelector(".form-input").value;
@@ -7,11 +28,15 @@ itemInput.addEventListener("click", (e) => {
     return;
   } else {
     const list = document.createElement("li");
-    list.innerHTML = `
-      ${val}
-      <button class="remove-item btn-link text-red">
-        <i class="fa-solid fa-xmark"></i>
-      </button>`;
+    const button = document.createElement("button");
+    button.className = "remove-item btn-link text-red";
+    const text = document.createTextNode(val);
+    list.appendChild(text);
+    const icon = document.createElement("i");
+    icon.className = "fa-solid fa-xmark";
+    icon.addEventListener("click", removeItem);
+    button.appendChild(icon);
+    list.appendChild(button);
     document.querySelector("ul").appendChild(list);
   }
 });
